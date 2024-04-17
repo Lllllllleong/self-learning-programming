27cfe3871e6bf8182a2c2148e5e7b4990a06f9d9
Leong Yin Chan <u6858120@anu.edu.au>
2024-04-17 17:54:48 +1000

    Learning Parsing Medium - Different Ways to Add Parentheses

---
+++ src/src/Leetcode3.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Leetcode3 {
+
+    public List<Integer> diffWaysToCompute(String expression) {
+        int n = expression.length();
+        List<Integer> l = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            char c = expression.charAt(i);
+            if (c == '*' || c == '+' || c == '-') {
+                List<Integer> leftCombinations = diffWaysToCompute(expression.substring(0,i));
+                List<Integer> rightCombinations = diffWaysToCompute(expression.substring(i+1));
+                for (Integer I : leftCombinations) {
+                    for (Integer II : rightCombinations) {
+                        switch (c) {
+                            case '*' -> l.add(I * II);
+                            case '-' -> l.add(I - II);
+                            case '+' -> l.add(I + II);
+                        }
+                    }
+                }
+            }
+        }
+        //Base case: If l is empty, then expression is an integer
+        if (l.size() == 0) {
+            l.add(Integer.valueOf(expression));
+        }
+        return l;
+    }
+
+
+}
