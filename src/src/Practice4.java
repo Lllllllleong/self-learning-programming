a0b60b9c64eb7cfde328f124f3c9421393fc8864
Leong Yin Chan <u6858120@anu.edu.au>
2024-06-01 11:05:36 +1000

    LC Easy - Build Array from Permutation

--- src/src/Practice4.java
+++ src/src/Practice4.java
@@ -245,6 +245,14 @@ public class Practice4 {
         }
         return output;
     }
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+        for (int i = 0; i < n; i++) {
+            output[i] = nums[nums[i]];
+        }
+        return output;
+    }
 
 
 
