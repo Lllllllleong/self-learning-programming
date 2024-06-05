d5428466ca37524d641327b80881aea219fd179a
Leong Yin Chan <u6858120@anu.edu.au>
2024-06-05 11:17:56 +1000

    HR Medium - LIS Learned about Arrays.binarySearch

--- src/src/Practice4.java
+++ src/src/Practice4.java
@@ -749,7 +749,44 @@ public class Practice4 {
         return sequenceCounter;
     }
 
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        int length = 0;
+        for (int num : nums) {
+            int index = Arrays.binarySearch(dp, 0, length, num);
+            if (index < 0) {
+                index = -(index + 1);
+            }
+            dp[index] = num;
+            if (index == length) {
+                length++;
+            }
+        }
+        return length;
+    }
 
+    public static int longestIncreasingSubsequence(List<Integer> arr) {
+        if (arr == null || arr.size() == 0) {
+            return 0;
+        }
+        int n = arr.size();
+        Integer[] dpArray = new Integer[n];
+        int length = 0;
+        for (Integer num : arr) {
+            int index = Arrays.binarySearch(dpArray, 0, length, num);
+            if (index < 0) {
+                index = -(index + 1);
+            }
+            dpArray[index] = num;
+            if (index == length) {
+                length++;
+            }
+        }
+        return length;
+    }
 
 
     public static void main(String[] args) {
