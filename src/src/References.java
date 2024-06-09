770c987811c50b9345594b9a4eca838ce969488b
Leong Yin Chan <u6858120@anu.edu.au>
2024-06-09 12:59:18 +1000

    LC Medium - Minimum Operations to Reduce X to Zero

--- src/src/References.java
+++ src/src/References.java
@@ -1,15 +1,18 @@
 
 import java.util.*;
+
 class Seed {
     int x;
     int y;
     static int totalSeedCount = 0;
     List<Seed> next = new ArrayList<>();
+
     public Seed(int x, int y) {
         this.x = x;
         this.y = y;
         totalSeedCount++;
     }
+
     @Override
     public String toString() {
         return "Seed{" +
@@ -19,67 +22,6 @@ class Seed {
 }
 
 public class References {
-
-
-
-    //SHORTCUTS
-
-    //itar
-    //ritar
-    //itco
-    //Max and min
-    //mx
-    //mn
-
-    //Last element
-    //lst
-
-    public static void main(String[] args) {
-
-        int[][] prices = {{1,4,2},{2,2,7},{2,1,3},{3,2,10},{1,4,2},{4,1,3}};
-        Arrays.sort(prices, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                double first = (double) a[2] / (a[0] * a[1]);
-                double second = (double) b[2] / (b[0] * b[1]);
-                if (second > first) return 1;
-                else if (first > second) return -1;
-                else return 0;
-            }
-        });
-
-
-        Arrays.sort(prices, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                int a1 = a[0];
-                int a2 = a[1];
-                int b1 = b[0];
-                int b2 = b[1];
-                if (a1 != b1) return (a1-b1);
-                return (a2-b2);
-            }
-        });
-
-
-        for (int[] intArr : prices) {
-            System.out.println(Arrays.toString(intArr));
-        }
-
-
-        //Int[] to deque
-//        Deque<Integer> queue = new ArrayDeque<>(Arrays.stream(nums).boxed().toList());
-
-
-        //Int array sum
-//        int sum = Arrays.stream(nums).boxed().reduce(0, (a, b) -> a + b);
-
-
-
-        //TreeMap
-        //Ceiling key : Returns the least key greater than or equal to the given key, or null if there is no such key.
-        //Floor key : Returns the greatest key less than or equal to the given key, or null if there is no such key.
-
-
-
 
         /*
             Static: Used when you dont care about the instance
@@ -95,134 +37,187 @@ public class References {
          */
 
 
-        //Map merge
-//          Increment hashmap value by 1 if it exists, if not, set it to 1
-        HashMap<Integer, Integer> map = new HashMap<>();
-        HashMap<Integer, List<Integer>> indexMap = new HashMap<>();
-        Integer key = 3;
-        Integer value = 3;
-        map.put(1, value);
+    //SHORTCUTS
+
+    //itar
+    //ritar
+    //itco
+    //Max and min
+    //mx
+    //mn
+
+    //Last element
+    //lst
 
 
-        map.merge(key, 1, Integer::sum);
+    public static void main(String[] args) {
+        // Section 1: Sorting Examples
+        sortExamples();
 
-        indexMap.merge(1, new ArrayList<>(List.of(1)), (a, b) -> {a.addAll(b); return a;});
+        // Section 2: Integer and Array Conversions
+        integerAndArrayConversions();
 
+        // Section 3: HashMap Operations
+        hashMapOperations();
 
+        // Section 4: Queue Examples
+        queueExamples();
 
+        // Section 5: Miscellaneous Examples
+        miscellaneousExamples();
+    }
 
-//          Sort based on hashmap values
-        //List of the keys
-        List<Integer> keyList = new ArrayList<>(map.keySet());
-        //Sort the list based on hashmap value
-        Collections.sort(keyList, new Comparator<Integer>() {
-            public int compare(Integer a, Integer b) {
-                return (map.get(b) - map.get(a));
+    // Section 1: Sorting Examples
+    public static void sortExamples() {
+        System.out.println("=== Sorting Examples ===");
+
+        int[][] prices = {{1, 4, 2}, {2, 2, 7}, {2, 1, 3}, {3, 2, 10}, {1, 4, 2}, {4, 1, 3}};
+
+        // Sort by a custom comparator
+        Arrays.sort(prices, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                double first = (double) a[2] / (a[0] * a[1]);
+                double second = (double) b[2] / (b[0] * b[1]);
+                return Double.compare(second, first);
             }
         });
 
+        // Sort by first then by second element
+        Arrays.sort(prices, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                if (a[0] != b[0]) return Integer.compare(a[0], b[0]);
+                return Integer.compare(a[1], b[1]);
+            }
+        });
 
+        // Print sorted arrays
+        for (int[] intArr : prices) {
+            System.out.println(Arrays.toString(intArr));
+        }
+    }
 
-        //Get hashmap key with largest value
-//        Key key = Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
-        // Collections.min(map.entrySet(), Map.Entry.comparingByValue()).getKey();
+    // Section 2: Integer and Array Conversions
+    public static void integerAndArrayConversions() {
+        System.out.println("=== Integer and Array Conversions ===");
 
-        //Max value in hashmap
-        int maxValueInMap = (Collections.max(map.values()));
-
-
-//          Sort based on string length. Lambda shortened expression of comparator
-//        Collections.sort(words, (a, b) -> {
-//            if (a.length() != b.length()) {
-//                return a.length() - b.length(); // Sort by length
-//            }
-//            return a.compareTo(b); // If lengths are the same, sort lexicographically
-//        });
-
-
-//        Char to int
+        // Char to int
         String n = "123";
         int i = 0;
         int x = n.charAt(i) - '0';
+        System.out.println("Char to int: " + x);
 
-
-//        Map char to an int
-//        a = 0, b = 1,...,z = 25
-//        A = -32, B = -31,...,Z = -7
+        // Map char to an int (a = 0, b = 1, ..., z = 25)
         n = "abc";
         x = n.charAt(0) - 'a';
-        System.out.println(x);
+        System.out.println("Char to int (a=0): " + x);
 
-
-        //Int to binary
-//        Integer.toBinaryString(int i)
-
-//        PriorityQueue
-//        Insertion order is not retained, the order of the queue is based on the specified comparator
-//        From smallest to biggest:
-        PriorityQueue<Integer> PQ = new PriorityQueue<>();
-//        From biggest to smallest:
-        PriorityQueue<Integer> PQ2 = new PriorityQueue<>(Collections.reverseOrder());
-
-        PriorityQueue<Integer> queue = new PriorityQueue<>((a,b) -> (a-b));
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-                Comparator.comparingInt((int[] a) -> a[1])
-                        .thenComparingInt(a -> a[0])
-        );
-
-
-
-//        Condensed for loop for strings
-        String s = "abcdefg";
-        for (char c : s.toCharArray()) {
-        }
-
-
-//        Double int arrays
+        // Int[] to List
         int[] a = {1, 2, 3};
-        int[] b = {4, 5, 6};
-        int[][] doubleArray = {a, b};
-//        doubleArray[0][0] will be 1
-//        doubleArray[1][0] will be 4
-//        doubleArray[y][x]
-//        doubleArray.length will be 2
+        List<Integer> list = new ArrayList<>(Arrays.stream(a).boxed().toList());
 
-        //Sub array
-        int[] subArray = Arrays.copyOfRange(a, 0, 0);
+        // List to int[]
+        int[] output = list.stream().mapToInt(Integer::intValue).toArray();
 
+        //int to binary string
+        String binaryString = Integer.toBinaryString(i);
 
-//        Instantiating List using Arrays.asList()
-        List<Integer> list = Arrays.asList(1, 2, 3);
+    }
 
+    // Section 3: HashMap Operations
+    public static void hashMapOperations() {
+        System.out.println("=== HashMap Operations ===");
 
+        // Increment HashMap value by 1 if it exists, if not, set it to 1
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.merge(3, 1, Integer::sum);
 
-        //Int[] to list
-        // Create a mutable list instead of an immutable one.
-        List<Integer> listt = new ArrayList<>(Arrays.stream(a).boxed().toList());
+        // HashMap with List values
+        HashMap<Integer, List<Integer>> indexMap = new HashMap<>();
+        indexMap.merge(1, new ArrayList<>(List.of(1)), (a, b) -> {
+            a.addAll(b);
+            return a;
+        });
 
-        //List to int array
-//        int[] output = list.stream().mapToInt(i->i).toArray();
+        // Sort HashMap keys based on values
+        List<Integer> keyList = new ArrayList<>(map.keySet());
+        Collections.sort(keyList, (a, b) -> Integer.compare(map.get(b), map.get(a)));
 
-
-        //Sort by reverse order
-        Collections.sort(listt, Collections.reverseOrder());
-
-        //Sort a set
-//        Set<Integer> keySet = treeMapKey.keySet();
-//        List<Integer> listSet = new ArrayList<>(keySet);
-//        Collections.sort(listSet, Collections.reverseOrder());
-
-
-//        Finding the middle of a linkedlist, use tortoise and hare method.
-
-
-
-
-
+        // Get HashMap key with largest value
+        Integer maxValueInMap = Collections.max(map.values());
+        Integer keyWithMaxValue = Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
 
 
     }
 
+    // Section 4: Queue Examples
+    public static void queueExamples() {
+        System.out.println("=== Queue Examples ===");
+        int[] a = new int[100];
+
+        //int[] to deque
+        Deque<Integer> queue = new ArrayDeque<>(Arrays.stream(a).boxed().toList());
+
+
+        // PriorityQueue from smallest to biggest
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(3);
+        pq.add(1);
+        pq.add(2);
+        System.out.println("PriorityQueue (smallest to biggest): " + pq);
+
+        // PriorityQueue from biggest to smallest
+        PriorityQueue<Integer> pq2 = new PriorityQueue<>(Collections.reverseOrder());
+        pq2.add(3);
+        pq2.add(1);
+        pq2.add(2);
+        System.out.println("PriorityQueue (biggest to smallest): " + pq2);
+
+        // PriorityQueue with custom comparator
+        PriorityQueue<int[]> pq3 = new PriorityQueue<>(Comparator.comparingInt((int[] z) -> z[1]).thenComparingInt(z -> z[0]));
+        pq3.add(new int[]{1, 3});
+        pq3.add(new int[]{2, 2});
+        pq3.add(new int[]{3, 1});
+        while (!pq3.isEmpty()) {
+            System.out.println("PriorityQueue with custom comparator: " + Arrays.toString(pq3.poll()));
+        }
+    }
+
+    // Section 5: Miscellaneous Examples
+    public static void miscellaneousExamples() {
+        System.out.println("=== Miscellaneous Examples ===");
+
+        // Sub array
+        int[] a = {1, 2, 3};
+        int[] subArray = Arrays.copyOfRange(a, 0, 2);
+        System.out.println("Sub array: " + Arrays.toString(subArray));
+
+        // Instantiating List using Arrays.asList()
+        List<Integer> list = Arrays.asList(1, 2, 3);
+
+        // Find middle of a LinkedList (Tortoise and Hare method)
+        LinkedList<Integer> linkedList = new LinkedList<>(List.of(1, 2, 3, 4, 5));
+        findMiddleOfLinkedList(linkedList);
+    }
+
+    public static void findMiddleOfLinkedList(LinkedList<Integer> list) {
+        System.out.println("=== Finding Middle of LinkedList ===");
+
+        ListIterator<Integer> slow = list.listIterator();
+        ListIterator<Integer> fast = list.listIterator();
+
+        while (fast.hasNext()) {
+            fast.next();
+            if (fast.hasNext()) {
+                fast.next();
+                slow.next();
+            }
+        }
+
+        System.out.println("Middle of LinkedList: " + (slow.hasNext() ? slow.next() : "List is empty"));
+    }
+
+
 }
+
+
 
