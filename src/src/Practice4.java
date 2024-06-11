e9aca39b9ba66bf36f29b5298bfdbab9edbc45b8
Leong Yin Chan <u6858120@anu.edu.au>
2024-06-11 15:04:04 +1000

    LC Easy - Find Greatest Common Divisor of Array

--- src/src/Practice4.java
+++ src/src/Practice4.java
@@ -1149,6 +1149,16 @@ public class Practice4 {
     }
 
 
+    public int findGCD(int[] nums) {
+        Arrays.sort(nums);
+        int a = nums[0];
+        int b = nums[nums.length-1];
+        for (int i = a; i >= 1; i--) {
+            if (a%i == 0 && b%i == 0) return i;
+        }
+        return -1;
+    }
+
 
     public static void main(String[] args) {
         int[] tasks = {10,6,6,8,3,7};
