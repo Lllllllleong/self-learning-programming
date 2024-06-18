87fc8a2d702bd223e6c0e10a63e7b2b863ead05e
Leong Yin Chan <u6858120@anu.edu.au>
2024-06-19 00:42:55 +0800

    LC Hard - Median of Two Sorted Arrays Double binary search

--- src/src/Practice4.java
+++ src/src/Practice4.java
@@ -1813,6 +1813,38 @@ public class Practice4 {
 
 
 
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int m = nums1.length;
+        int n = nums2.length;
+        int low = 0, high = m;
+        while (low <= high) {
+            int i = (low + high) / 2;
+            int j = (m + n + 1) / 2 - i;
+            int nums1LeftMax = (i == 0) ? Integer.MIN_VALUE : nums1[i - 1];
+            int nums1RightMin = (i == m) ? Integer.MAX_VALUE : nums1[i];
+            int nums2LeftMax = (j == 0) ? Integer.MIN_VALUE : nums2[j - 1];
+            int nums2RightMin = (j == n) ? Integer.MAX_VALUE : nums2[j];
+            if (nums1LeftMax <= nums2RightMin && nums2LeftMax <= nums1RightMin) {
+                if ((m + n) % 2 == 0) {
+                    return ((double) Math.max(nums1LeftMax, nums2LeftMax) + Math.min(nums1RightMin, nums2RightMin)) / 2;
+                } else {
+                    return Math.max(nums1LeftMax, nums2LeftMax);
+                }
+            } else if (nums1LeftMax > nums2RightMin) {
+                high = i - 1;
+            } else {
+                low = i + 1;
+            }
+        }
+        return -1d;
+    }
+
+
+
+
     public static void main(String[] args) {
 
         int[] nums = {3, 5, 0, 3, 4};
