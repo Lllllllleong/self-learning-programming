671d1fc35c4e340f0002cde1cf6c2b3d257a6b1c
Leong Yin Chan <u6858120@anu.edu.au>
2024-06-19 14:48:39 +0800

    LC Hard - Maximum Score from Performing Multiplication Operations

--- src/src/Practice4.java
+++ src/src/Practice4.java
@@ -2034,6 +2034,22 @@ public class Practice4 {
 
 
 
+    public int maximumScore(int[] nums, int[] multipliers) {
+        int n = nums.length;
+        int m = multipliers.length;
+        int[][] dp = new int[m + 1][m + 1];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int left = i; left >= 0; left--) {
+                int mult = multipliers[i];
+                int right = n - 1 - (i - left);
+                dp[i][left] = Math.max(
+                        nums[left] * mult + dp[i + 1][left + 1],
+                        nums[right] * mult + dp[i + 1][left]
+                );
+            }
+        }
+        return dp[0][0];
+    }
 
 
 
