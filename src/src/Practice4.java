8f86e511b4cc12455ca02ccf4dc28a2939ce52ff
Leong Yin Chan <u6858120@anu.edu.au>
2024-06-22 22:13:16 +0800

    LC Medium - Inorder Successor in BST Largest Number

--- src/src/Practice4.java
+++ src/src/Practice4.java
@@ -2776,6 +2776,39 @@ public class Practice4 {
         }
     }
 
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null) return null;
+        if (root.val <= p.val) {
+            return inorderSuccessor(root.right, p);
+        } else {
+            TreeNode tn = inorderSuccessor(root.left, p);
+            return (tn == null) ? root : tn;
+        }
+    }
+
+    public String largestNumber(int[] nums) {
+        String[] strNums = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strNums[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(strNums, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                String order1 = a + b;
+                String order2 = b + a;
+                return order2.compareTo(order1);
+            }
+        });
+        if (strNums[0].equals("0")) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String str : strNums) {
+            sb.append(str);
+        }
+        return sb.toString();
+    }
+
 
 
 
