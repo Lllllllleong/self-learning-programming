c4b21d2f45f49b43da21e8e0b8efa9943999466e
Leong Yin Chan <u6858120@anu.edu.au>
2024-07-01 01:54:49 +0800

    LC Medium - threeSumSmaller

--- src/src/Practice4.java
+++ src/src/Practice4.java
@@ -3526,6 +3526,27 @@ public class Practice4 {
     }
 
 
+
+
+    public int threeSumSmaller(int[] nums, int target) {
+        int count = 0;
+        Arrays.sort(nums);
+        int len = nums.length;
+        for(int i=0; i<len-2; i++) {
+            int left = i+1, right = len-1;
+            while(left < right) {
+                if(nums[i] + nums[left] + nums[right] < target) {
+                    count += right-left;
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
         int[][] workers = {{0, 0}, {2, 1}};
         int[][] bikes = {{1, 2}, {3, 3}};
