4c0b9800a22a1baaf583472bd4e1a313b40e5a6c
Leong Yin Chan <u6858120@anu.edu.au>
2024-07-31 18:58:53 +1000

    Medium - Maximum Array Hopping Score I
    Greedy Algo solution

--- src/src/Practice5.java
+++ src/src/Practice5.java
@@ -2819,13 +2819,13 @@ public class Practice5 {
 
     public int maxScore(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i+1; j < n; j++) {
-                dp[i] = Math.max(dp[i], ((j-i) * nums[j]) + dp[j]);
-            }
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = n - 1; i > 0; i--) {
+            max = Math.max(max, nums[i]);
+            sum += max;
         }
-        return dp[0];
+        return sum;
     }
 
 
