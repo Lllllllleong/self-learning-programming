cf1dc65ae641537eb70a57a6173e0283f29e3af6
Leong Yin Chan <u6858120@anu.edu.au>
2024-08-06 19:37:37 +1000

    Medium - Find the Substring With Maximum Cost

--- src/src/Practice5.java
+++ src/src/Practice5.java
@@ -5,6 +5,160 @@ import java.util.*;
 
 public class Practice5 {
 
+    public static List<List<String>> convertToListOfLists(String[][] array) {
+        List<List<String>> listOfLists = new ArrayList<>();
+
+        for (String[] subArray : array) {
+            listOfLists.add(Arrays.asList(subArray));
+        }
+
+        return listOfLists;
+    }
+    public static int[] stringToArray1D(String input) {
+        String[] numberStrings = input.substring(1, input.length() - 1).split(",");
+        int[] numbers = new int[numberStrings.length];
+        for (int i = 0; i < numberStrings.length; i++) {
+            numbers[i] = Integer.parseInt(numberStrings[i]);
+        }
+        return numbers;
+    }
+    // Method for 2D array
+    public static int[][] stringToArray2D(String input) {
+        // Remove outer brackets and split into individual array strings
+        String[] arrayStrings = input.substring(1, input.length() - 1).split("(?<=\\]),\\[");
+        // Prepare a list to hold the final arrays
+        List<int[]> arraysList = new ArrayList<>();
+        for (String arrayString : arrayStrings) {
+            // Remove brackets from each array string and split by comma
+            String[] numberStrings = arrayString.replaceAll("[\\[\\]]", "").split(",");
+            int[] numbers = new int[numberStrings.length];
+            for (int i = 0; i < numberStrings.length; i++) {
+                numbers[i] = Integer.parseInt(numberStrings[i]);
+            }
+            arraysList.add(numbers);
+        }
+        // Convert list to array
+        int[][] result = new int[arraysList.size()][];
+        for (int i = 0; i < arraysList.size(); i++) {
+            result[i] = arraysList.get(i);
+        }
+        return result;
+    }
+    public class Node {
+        int val;
+        Node left;
+        Node right;
+        Node random;
+
+        Node() {
+        }
+
+        Node(int val) {
+            this.val = val;
+        }
+
+        Node(int val, Node left, Node right, Node random) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+            this.random = random;
+        }
+    }
+    public class NodeCopy {
+        int val;
+        NodeCopy left;
+        NodeCopy right;
+        NodeCopy random;
+
+        NodeCopy() {
+        }
+
+        NodeCopy(int val) {
+            this.val = val;
+        }
+
+        NodeCopy(int val, NodeCopy left, NodeCopy right, NodeCopy random) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+            this.random = random;
+        }
+    }
+    class Interval {
+        public int start;
+        public int end;
+
+        public Interval() {
+        }
+
+        public Interval(int _start, int _end) {
+            start = _start;
+            end = _end;
+        }
+    }
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Practice5 practice5 = new Practice5();
+
+    }
+
+
+
+    public int maximumCostSubstring(String s, String chars, int[] vals) {
+        int[] charValue = new int[26];
+        for (int i = 0; i < 26; i++) charValue[i] = i+1;
+        int v = vals.length;
+        char[] sChars = s.toCharArray();
+        char[] cChars = chars.toCharArray();
+        for (int i = 0; i < v; i++) charValue[cChars[i]-'a'] = vals[i];
+        int currentScore = 0;
+        int max = 0;
+        for (char c : sChars) {
+            currentScore += charValue[c-'a'];
+            if (currentScore < 0) {
+                currentScore = 0;
+            } else {
+                max = Math.max(max, currentScore);
+            }
+        }
+        return max;
+    }
+
+
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int n = numCourses;
@@ -3150,166 +3304,6 @@ public class Practice5 {
         return dp[0];
     }
 
-
-    /**
-     * Main Method
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     */
-    public static void main(String[] args) {
-        Practice5 practice5 = new Practice5();
-        var v = practice5.findStrobogrammatic(2);
-
-
-        int[] houses = {0, 2, 1, 2, 0};
-        int[][] cost = stringToArray2D("[[1,10],[10,1],[10,1],[1,10],[5,1]]");
-        int i = practice5.minCost(houses, cost, 5, 2, 3);
-        int[] arr = {1, 6, 1};
-        i = practice5.minSumOfLengths(arr, 7);
-    }
-
-    public static List<List<String>> convertToListOfLists(String[][] array) {
-        List<List<String>> listOfLists = new ArrayList<>();
-
-        for (String[] subArray : array) {
-            listOfLists.add(Arrays.asList(subArray));
-        }
-
-        return listOfLists;
-    }
-
-
-    public static int[] stringToArray1D(String input) {
-        String[] numberStrings = input.substring(1, input.length() - 1).split(",");
-        int[] numbers = new int[numberStrings.length];
-        for (int i = 0; i < numberStrings.length; i++) {
-            numbers[i] = Integer.parseInt(numberStrings[i]);
-        }
-        return numbers;
-    }
-
-    // Method for 2D array
-    public static int[][] stringToArray2D(String input) {
-        // Remove outer brackets and split into individual array strings
-        String[] arrayStrings = input.substring(1, input.length() - 1).split("(?<=\\]),\\[");
-        // Prepare a list to hold the final arrays
-        List<int[]> arraysList = new ArrayList<>();
-        for (String arrayString : arrayStrings) {
-            // Remove brackets from each array string and split by comma
-            String[] numberStrings = arrayString.replaceAll("[\\[\\]]", "").split(",");
-            int[] numbers = new int[numberStrings.length];
-            for (int i = 0; i < numberStrings.length; i++) {
-                numbers[i] = Integer.parseInt(numberStrings[i]);
-            }
-            arraysList.add(numbers);
-        }
-        // Convert list to array
-        int[][] result = new int[arraysList.size()][];
-        for (int i = 0; i < arraysList.size(); i++) {
-            result[i] = arraysList.get(i);
-        }
-        return result;
-    }
-
-
-    public class Node {
-        int val;
-        Node left;
-        Node right;
-        Node random;
-
-        Node() {
-        }
-
-        Node(int val) {
-            this.val = val;
-        }
-
-        Node(int val, Node left, Node right, Node random) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-            this.random = random;
-        }
-    }
-
-    public class NodeCopy {
-        int val;
-        NodeCopy left;
-        NodeCopy right;
-        NodeCopy random;
-
-        NodeCopy() {
-        }
-
-        NodeCopy(int val) {
-            this.val = val;
-        }
-
-        NodeCopy(int val, NodeCopy left, NodeCopy right, NodeCopy random) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-            this.random = random;
-        }
-    }
-
-
-    class Interval {
-        public int start;
-        public int end;
-
-        public Interval() {
-        }
-
-        public Interval(int _start, int _end) {
-            start = _start;
-            end = _end;
-        }
-    }
-
-
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
-
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
 
 
 }
