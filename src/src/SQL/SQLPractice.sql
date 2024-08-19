3b8eb7929506324fab0ce3215d2baf9087db849e
Leong Yin Chan <u6858120@anu.edu.au>
2024-08-19 17:40:10 +1000

    SQL Easy - Find Followers Count

--- src/src/SQL/SQLPractice.sql
+++ src/src/SQL/SQLPractice.sql
@@ -2,6 +2,17 @@
 -- SQL Practice
 --
 SELECT
+    user_id,
+    COUNT(follower_id) as followers_count
+FROM
+    Followers
+GROUP BY
+    user_id
+ORDER BY
+    user_id ASC;
+
+
+SELECT
     class
 FROM
     Courses
