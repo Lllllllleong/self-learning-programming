8e94651e4dbfb9e0bdcfd08ae88aae59b12b8bf7
Leong Yin Chan <u6858120@anu.edu.au>
2024-09-09 15:04:01 +1000

    Medium - Largest Time for Given Digits

--- src/src/Practice6.java
+++ src/src/Practice6.java
@@ -142,6 +142,29 @@ public class Practice6 {
 
     }
 
+    public String largestTimeFromDigits(int[] arr) {
+        int maxHour = -1;
+        int maxMin = -1;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (j == i) continue;
+                for (int k = 0; k < 4; k++) {
+                    if (k == i || k == j) continue;
+                    int l = 6 - i - j - k;
+                    int hour = arr[i] * 10 + arr[j];
+                    int min = arr[k] * 10 + arr[l];
+                    if (hour < 24 && min < 60) {
+                        if (hour > maxHour || (hour == maxHour && min > maxMin)) {
+                            maxHour = hour;
+                            maxMin = min;
+                        }
+                    }
+                }
+            }
+        }
+        if (maxHour == -1) return "";
+        return String.format("%02d:%02d", maxHour, maxMin);
+    }
     public int validSubarrays(int[] nums) {
         int n = nums.length;
         int output = 0;
