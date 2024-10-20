2453d0799ba48eb672bb443e4ac570d86adc18de
Leong Yin Chan <u6858120@anu.edu.au>
2024-10-20 20:01:09 +1100

    LC Easy - Find the Encrypted String

--- src/src/Practice6.java
+++ src/src/Practice6.java
@@ -143,9 +143,16 @@ public class Practice6 {
     //
     ///////////////////////////////////////////////////////////////////////////
 
+    public String getEncryptedString(String s, int k) {
+        int n = s.length();
+        StringBuilder encrypted = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int newIndex = (i + k) % n;
+            encrypted.append(s.charAt(newIndex));
+        }
 
-
-
+        return encrypted.toString();
+    }
 
     public int[] resultsArray(int[][] queries, int k) {
         int n = queries.length;
