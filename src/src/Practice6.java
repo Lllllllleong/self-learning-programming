2f527742f05d55d4289688ce48e5d19e6df44882
Leong Yin Chan <u6858120@anu.edu.au>
2024-10-22 12:00:50 +1100

    Busy studying for final exams - LC Easy - Check If Two String Arrays are Equivalent

--- src/src/Practice6.java
+++ src/src/Practice6.java
@@ -143,7 +143,13 @@ public class Practice6 {
     //
     ///////////////////////////////////////////////////////////////////////////
 
-
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for (String s : word1) sb1.append(s);
+        for (String s : word2) sb2.append(s);
+        return sb1.toString().equals(sb2.toString());
+    }
 
     public int canBeTypedWords(String text, String brokenLetters) {
         boolean[] charFlags =  new boolean[26];
